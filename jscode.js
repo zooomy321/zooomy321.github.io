42eae3ea83601d206ea6821f69116773cd622fee
zooomy321 <aungminnthant33@gmail.com>
2025-04-12 15:18:05 +0800

    Test Test 1

---
+++ jscode.js
@@ -0,0 +1,4 @@
+function alert1() {
+    if (window.confirm("By continuing, you agree to submit your paper for grading, and acknowledge that this action cannot be undone.") == true){location.replace("end.html")}
+    
+}
